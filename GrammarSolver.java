2478cea8533e8e19abdaa7e372c7f9643ed07fac
JaysheelPandya <71728460+JaysheelPandya@users.noreply.github.com>
2024-01-05 21:47:47 -0800

    Add files via upload

---
+++ GrammarSolver.java
@@ -0,0 +1,80 @@
+// JaysheelPandya
+// CSE 143 DD with Xunmei Liu
+// Homework 5
+// The GrammarSolver class reads an input file with grammar in
+// Backus-Naur form and randomly generates elements of the grammar
+
+import java.util.*;
+
+public class GrammarSolver {
+
+    // Stores the grammar
+    private SortedMap<String, String[]> grammarStorage;
+
+    // PRE: The given grammar must not be empty
+    // (throws IllegalArgumentException otherwise).
+    // There should not be duplicate non-terminal symbols
+    // in the given grammar (throws IllegalArgumentException otherwise).
+    // POST: Takes the given grammar and stores it conveniently,
+    // separated by non-terminal symbols and their given rules
+    public GrammarSolver(List<String> grammar) {
+        if (grammar.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        grammarStorage = new TreeMap<>();
+        for (String s : grammar)  {
+            String[] parts = s.split("::=");
+            if (grammarStorage.containsKey(parts[0])) {
+                throw new IllegalArgumentException();
+            }
+            String[] rules = parts[1].split("[|]");
+            grammarStorage.put(parts[0], rules);
+        }
+    }
+
+    // Returns true if the stored grammar contains the
+    // given non-terminal symbol
+    public boolean grammarContains(String symbol) {
+        return grammarStorage.containsKey(symbol);
+    }
+
+    // PRE: The stored grammar must contain the given symbol,
+    // and the given times to generate must be greater than or equal to 0
+    // (throws IllegalArgumentException otherwise).
+    // POST: Creates and returns a randomly generated grammar the
+    // given number of times
+    public String[] generate(String symbol, int times) {
+        if (!grammarContains(symbol) || times < 0) {
+            throw new IllegalArgumentException();
+        }
+        String[] results = new String[times];
+        for (int i = 0; i < times; i++) {
+            results[i] = generateGrammar(symbol).trim();
+        }
+        return results;
+    }
+
+    // Creates and returns a randomly generated grammar
+    // using the given symbol
+    private String generateGrammar(String symbol) {
+        String result = "";
+        String[] rules = grammarStorage.get(symbol);
+        Random rand = new Random();
+        int chosenRule = rand.nextInt(rules.length);
+        String[] parts = rules[chosenRule].trim().split("[ \t]+");
+        for (String part : parts) {
+            if (grammarStorage.containsKey(part)) {
+                result += generateGrammar(part);
+            }
+            else {
+                result += part + " ";
+            }
+        }
+        return result;
+    }
+
+    // Returns all the non-terminal symbols in the stored grammar
+    public String getSymbols() {
+        return grammarStorage.keySet().toString();
+    }
+}
---
+++ TestE.java
@@ -0,0 +1,14 @@
+// Short program to test whether the expressions generated using the file
+// sentence2.txt are legal.  They can often be very long.  You should include
+// the expression as indicated below and you should be able to compile the
+// program.  It might throw an exception when run, but it should compile.
+
+import static java.lang.Math.*;
+
+public class TestE {
+    public static void main(String[] args) {
+        double x = 3;
+        double y = 4;
+        double z = ( x * x + - x ) / pow ( - 92 % pow ( ( 0 % ( cos ( 1 ) ) - 0 * 1 ) - - 42 * y * min ( y / ( x ) * 92 % - sin ( y ) / 1 , ( 0 % 42 / y + max ( 42 * ( - min ( ( 0 ) , 1 + y + min ( y - max ( x , - pow ( 92 , - sin ( - - 42 + 1 / 0 ) ) ) - ( min ( 42 , y + - ( - 42 / y ) + pow ( max ( x - 92 / y - 1 + pow ( x , ( 92 ) ) - 42 , 42 - ( y ) ) % 42 , y ) ) ) , y ) ) * 1 ) / pow ( 42 + - 42 , 0 % x + 0 ) , max ( 1 % tan ( ( y / y ) ) , 92 * y ) - 0 ) ) % 42 ) * - x / 0 % 42 - y , pow ( tan ( y * - pow ( 92 + y + - 1 + x % cos ( y ) / y / 1 , 0 / - 92 * max ( min ( 0 , 42 / abs ( ( 1 - 42 ) ) ) + ( pow ( x / 1 , 1 % - ( min ( pow ( min ( 0 * 0 + - sin ( tan ( y ) ) + y , max ( y * 92 , 1 % ( ( 1 ) ) % - 92 ) + sqrt ( 92 % - - sin ( sqrt ( - 0 / min ( 42 , 42 * 1 * y * abs ( pow ( ( tan ( min ( 42 * 42 % 42 , - ( sin ( 92 / x + abs ( 0 ) - - 1 ) + 0 ) * 0 ) ) - abs ( 92 * ( 0 ) * ( 42 * y ) ) - 42 + y + 42 / 0 ) , 92 ) / x ) % y ) ) ) ) + ( y ) ) / cos ( - 42 ) , min ( 0 + x , 0 / ( 42 % - - 0 % 92 + y ) - - 42 ) ) / y % ( 42 ) , y + 1 - y + 92 ) * 42 ) ) - 1 ) / 1 , - x ) - 1 - tan ( pow ( 42 + 1 + sin ( - pow ( x , x % - ( 92 ) / max ( min ( ( max ( 0 , 92 ) ) , 92 ) * 0 - 0 , 1 % x * ( ( x * - y % 0 - 0 % - x * ( 1 ) / ( 42 ) / ( 42 ) + x ) * 92 ) ) ) ) * ( 1 * min ( ( abs ( y ) + sin ( 0 - 42 % 1 - 42 * 42 ) ) % x , 1 ) ) * ( 92 ) , min ( 1 + - 0 * min ( 1 , tan ( - 0 ) ) - 1 , abs ( 42 ) * tan ( pow ( max ( ( ( 0 - 1 % 42 % 0 * y ) * y % - 92 ) - 1 * x + ( 1 * max ( 92 , x ) ) - 92 , 92 ) + sqrt ( 42 % 1 ) , 92 % 1 % 0 ) ) - 42 + ( sin ( y / - cos ( y ) * 92 % 0 / 42 ) ) ) ) ) - 92 ) ) * y * 0 , 92 ) - 92 % y % y ) % ( y ) , ( 1 ) );
+    }
+}
